8e6f7691144d50da06b131863a94b1fac67106d2
Tim Bernhard <tim@bernhard-webstudio.ch>
2020-10-23 07:48:00 +0200

    Upgrade dependencies and change breaking changes

--- src/Transport/Events/ResponseEvent.php
+++ src/Transport/Events/ResponseEvent.php
@@ -11,7 +11,7 @@
 
 namespace CL\Slack\Transport\Events;
 
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 /**
  * @author Cas Leentfaar <info@casleentfaar.com>
